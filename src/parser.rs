2bbe5f3cc76cff14feb9f23c68920ad4c3df14f2
Dan Gohman <sunfish@mozilla.com>
2018-03-09 12:57:55 -0800

    Remove the size field from BrTable.
    This shrinks the size of Operator from 32 bytes to 24 bytes. It
    does mean that `read_table` can't use `with_capacity`, however
    performance-sensitive uses can use BrTableIterator to process the
    table elements to avoid allocating a Vec altogether.

--- src/parser.rs
+++ src/parser.rs
@@ -154,7 +154,6 @@ pub struct MemoryImmediate {
 /// A br_table entries representation.
 #[derive(Debug)]
 pub struct BrTable<'a> {
-    pub size: usize,
     buffer: &'a [u8],
 }
 
@@ -176,12 +175,11 @@ impl<'a> BrTable<'a> {
     /// ```
     pub fn read_table(&self) -> (Vec<u32>, u32) {
         let mut reader = BinaryReader::new(self.buffer);
-        let mut table = Vec::with_capacity(self.size);
-        for _ in 0..self.size {
+        let mut table = Vec::new();
+        while !reader.eof() {
             table.push(reader.read_var_u32().unwrap());
         }
-        let default_target = reader.read_var_u32().unwrap();
-        assert!(reader.eof());
+        let default_target = table.pop().unwrap();
         (table, default_target)
     }
 }
@@ -201,7 +199,6 @@ impl<'a> BrTable<'a> {
 /// ```
 pub struct BrTableIterator<'a> {
     reader: BinaryReader<'a>,
-    left: usize,
 }
 
 impl<'a> IntoIterator for &'a BrTable<'a> {
@@ -211,7 +208,6 @@ impl<'a> IntoIterator for &'a BrTable<'a> {
     fn into_iter(self) -> Self::IntoIter {
         BrTableIterator {
             reader: BinaryReader::new(self.buffer),
-            left: self.size + 1,
         }
     }
 }
@@ -220,10 +216,9 @@ impl<'a> Iterator for BrTableIterator<'a> {
     type Item = u32;
 
     fn next(&mut self) -> Option<u32> {
-        if self.left == 0 {
+        if self.reader.eof() {
             return None;
         }
-        self.left -= 1;
         Some(self.reader.read_var_u32().unwrap())
     }
 }
@@ -871,7 +866,6 @@ impl<'a> BinaryReader<'a> {
         }
         self.skip_var_32()?;
         Ok(BrTable {
-               size: targets_len,
                buffer: &self.buffer[start..self.position],
            })
     }
