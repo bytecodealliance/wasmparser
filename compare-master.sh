99ac9c7bb2481e0f5505f56a4dfa625e7e39a9c0
Yury Delendik <ydelendik@mozilla.com>
2020-02-05 11:45:42 -0600

    Try to fix github actions run

--- compare-master.sh
+++ compare-master.sh
@@ -1,11 +1,11 @@
 #/bin/bash
 
 # record current bench results
-cargo bench --bench benchmark -- --noplot --save-baseline after
+cargo bench --bench benchmark -- --noplot --save-baseline after --sample-size 30
 
 # switch to master and record its bench results
-git checkout master && \
-cargo bench --bench benchmark -- --noplot --save-baseline before
+git checkout -f --recurse-submodules master && \
+cargo bench --bench benchmark -- --noplot --save-baseline before --sample-size 30
 
 # compare
 cargo install critcmp --force && \
