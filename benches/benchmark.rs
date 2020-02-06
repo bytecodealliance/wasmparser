0dad3109f62373c88414214eb379b817d52cd6fe
Yury Delendik <ydelendik@mozilla.com>
2020-02-06 10:10:20 -0600

    Fix long running benchmarking

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,10 +1,3 @@
-pub fn read_file_data(path: &PathBuf) -> Vec<u8> {
-    let mut data = Vec::new();
-    let mut f = File::open(path).ok().unwrap();
-    f.read_to_end(&mut data).unwrap();
-    data
-}
-
 const VALIDATOR_CONFIG: Option<ValidatingParserConfig> = Some(ValidatingParserConfig {
     operator_config: OperatorValidatorConfig {
         enable_threads: true,
@@ -25,8 +18,7 @@ use wasmparser::{
     ValidatingParserConfig, WasmDecoder,
 };
 
-use std::fs::{read_dir, File};
-use std::io::Read;
+use std::fs::{read, read_dir};
 use std::path::PathBuf;
 
 fn read_all_wasm<'a, T>(mut d: T)
@@ -42,50 +34,64 @@ where
     }
 }
 
-fn it_works_benchmark(c: &mut Criterion) {
+static TEST_FILES: &[&'static str] = &[
+    "atomic.0.wasm",
+    "block.0.wasm",
+    "br_table.0.wasm",
+    "br_if.0.wasm",
+    "call.0.wasm",
+    "call_indirect.0.wasm",
+    "conversions.0.wasm",
+    "elem.0.wasm",
+    "fac.0.wasm",
+    "float_literals.0.wasm",
+    "func.0.wasm",
+    "get_local.0.wasm",
+    "globals.0.wasm",
+    "int_exprs.0.wasm",
+    "if.0.wasm",
+    "imports.0.wasm",
+    "loop.0.wasm",
+    "memory_grow.0.wasm",
+    "return.0.wasm",
+    "select.0.wasm",
+    "simd.wasm",
+    "type.0.wasm",
+];
+
+fn read_files_data() -> Vec<Vec<u8>> {
     let mut data: Vec<Vec<u8>> = vec![];
-    for entry in read_dir("tests").unwrap() {
-        let dir = entry.unwrap();
-        if !dir.file_type().unwrap().is_file() {
-            continue;
-        }
-        data.push(read_file_data(&dir.path()));
+    let path = PathBuf::from("tests");
+    for test_file in TEST_FILES {
+        let path = path.join(test_file);
+        data.push(read(&path).expect("wasm file data"));
     }
+    data
+}
+
+fn it_works_benchmark(c: &mut Criterion) {
+    let data = read_files_data();
     c.bench_function("it works benchmark", move |b| {
-        for d in &mut data {
+        for d in data.iter() {
             b.iter(|| read_all_wasm(Parser::new(d.as_slice())));
         }
     });
 }
 
 fn validator_not_fails_benchmark(c: &mut Criterion) {
-    let mut data: Vec<Vec<u8>> = vec![];
-    for entry in read_dir("tests").unwrap() {
-        let dir = entry.unwrap();
-        if !dir.file_type().unwrap().is_file() {
-            continue;
-        }
-        data.push(read_file_data(&dir.path()));
-    }
+    let data = read_files_data();
     c.bench_function("validator no fails benchmark", move |b| {
-        for d in &mut data {
+        for d in data.iter() {
             b.iter(|| read_all_wasm(ValidatingParser::new(d.as_slice(), VALIDATOR_CONFIG)));
         }
     });
 }
 
 fn validate_benchmark(c: &mut Criterion) {
-    let mut data: Vec<Vec<u8>> = vec![vec![]];
-    for entry in read_dir("tests").unwrap() {
-        let dir = entry.unwrap();
-        if !dir.file_type().unwrap().is_file() {
-            continue;
-        }
-        data.push(read_file_data(&dir.path()));
-    }
+    let data = read_files_data();
     c.bench_function("validate benchmark", move |b| {
-        for d in &mut data {
-            b.iter(|| validate(&d, VALIDATOR_CONFIG));
+        for d in data.iter() {
+            b.iter(|| validate(d.as_slice(), VALIDATOR_CONFIG));
         }
     });
 }
